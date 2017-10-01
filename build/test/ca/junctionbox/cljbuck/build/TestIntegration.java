8652fa31199b01fb01dc92646d0ba2dbec658474
Nathan Fisher <nfisher@junctionbox.ca>
2017-10-01 23:49:49 +0100

    Test failing build with buck-out/cache on Travis.

--- build/test/ca/junctionbox/cljbuck/build/TestIntegration.java
+++ build/test/ca/junctionbox/cljbuck/build/TestIntegration.java
@@ -8,6 +8,6 @@ import static org.junit.Assert.assertThat;
 public class TestIntegration {
     @Test
     public void Test_fail() {
-        assertThat("tests written: you've been a very naughty boy...", 0, is(0));
+        assertThat("tests written: you've been a very naughty boy...", 0, is(10));
     }
 }
